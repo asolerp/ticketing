93f16744c620ca725d3ee8ebaf903373bfda443b
Alberto <albertosolpal@gmail.com>
2020-07-20 07:54:37 +0200

    Working on ingress-ngix

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,28 @@
-function Home() {
-  return <h1>Home Page</h1>
+import axios from 'axios'
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser)
+  return <h1>Landing Page</h1>
 }
 
-export default Home
+LandingPage.getInitialProps = async () => {
+  if  (typeof window === undefined) {
+    // const { data } = await axios.get(
+    //   'https://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+    //     headers: {
+    //       Host: 'ticketing.dev'
+    //     }
+    //   }
+    // )
+    // return data
+    console.log('We are in the server')
+    // we are on the server
+  } else {
+    console.log('We are in the client')
+    // const { data } = await axios.get('/api/users/currentuser')
+    // return data
+  }
+  return {}
+}
+
+export default LandingPage
