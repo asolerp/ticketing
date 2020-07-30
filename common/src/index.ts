d67c3ec75e33e917f5384747c00855bfa86fc092
Alberto <albertosolpal@gmail.com>
2020-07-30 19:53:46 +0200

    "Updates"

--- common/src/index.ts
+++ common/src/index.ts
@@ -10,3 +10,5 @@ export * from './middlewares//error-handler'
 export * from './middlewares/require-auth'
 export * from './middlewares/validate-request'
 
+export * from './events/base-listener'
+export * from './events/base-publisher'
