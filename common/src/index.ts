9f034ad0424e8a9e0d3069c8432c817640f52e39
Alberto <albertosolpal@gmail.com>
2020-08-01 19:13:12 +0200

    "Updates"

--- common/src/index.ts
+++ common/src/index.ts
@@ -12,3 +12,6 @@ export * from './middlewares/validate-request'
 
 export * from './events/base-listener'
 export * from './events/base-publisher'
+export * from './events/subjects'
+export * from './events/ticket-created-event'
+export * from './events/ticket-updated-event'
